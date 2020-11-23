e1d0e700df0d32b8b2a4b4212763e44f1cccaa32
Aku Pasanen <akustic1@gmail.com>
2020-11-23 13:57:52 +0200

    Frontend lisätty projektiin

---
+++ README.md
@@ -0,0 +1,1 @@
+https://stark-everglades-68840.herokuapp.com/api/persons
---
+++ build/precache-manifest.5fb4cce7d5a005147bfa80babdbd002d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6753c4a02ea8609e62ed75c51b41782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01c9b6dc3f741c75ecc3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "581509edc098e8f19e97",
+    "url": "/static/js/2.13a90021.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13a90021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c9b6dc3f741c75ecc3",
+    "url": "/static/js/main.4492d6cb.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
